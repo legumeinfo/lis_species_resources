9de9183ec0102d5c298f28c76a0fed9a3f4ed1fe
sdash <sdash@ncgr.org>
2016-03-14 16:42:53 -0500

    QTL and Processing for external links added

--- lis_species_resources_page.tpl.php
+++ lis_species_resources_page.tpl.php
@@ -1,4 +1,14 @@
 <!-- Title and guide text  -->
+
+<style> 
+  .newspaper_col {
+   -webkit-columns: 100px 2; /* Chrome, Safari, Opera */
+   -moz-columns: 100px 2; /* Firefox */
+    columns: 100px 2;
+	    }
+</style>
+
+
 <div>
   <h1>Available resources for legume species at LIS</h1>
   <span></span>
@@ -21,7 +31,7 @@
     ?>
 </div>
 
-<div id="speciescontent">
+<div id="speciescontent"   class="newspaper_col">
     <?php
          echo $species_html;
     ?>
