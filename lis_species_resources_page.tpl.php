c04878c584d5a97b4ad075cba98afac6756bf7d1
sdash <sdash@ncgr.org>
2016-03-14 17:35:57 -0500

    Upto 5 extrnal links; columns don't break within a species.

--- lis_species_resources_page.tpl.php
+++ lis_species_resources_page.tpl.php
@@ -5,7 +5,18 @@
    -webkit-columns: 100px 2; /* Chrome, Safari, Opera */
    -moz-columns: 100px 2; /* Firefox */
     columns: 100px 2;
+
+    -webkit-column-break-inside: avoid;
+    page-break-inside: avoid;
+    break-inside: avoid;
 	    }
+    
+    .dl_no_break {
+    -webkit-column-break-inside: avoid; /* Chrome, Safari */
+    page-break-inside: avoid;           /* Theoretically FF 20+ */
+    break-inside: avoid-column;         /* IE 11 */
+    display:table;                      /* Actually FF 20+ */
+    }
 </style>
 
 
